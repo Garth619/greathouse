3b5305912795a9e02c7d3a5e79831ac0e9833f7d
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-14 09:44:32 -0700

    push

--- wp-content/themes/greathouse/footer.php
+++ wp-content/themes/greathouse/footer.php
@@ -1,5 +1,37 @@
 
 </section><!-- main -->
+
+<footer>
+	
+	<div class="inner_footer">
+		
+		<div class="locations">
+			
+			<span class="large_header">store locations</span><!-- large_header -->
+			
+			<div class="location">
+				
+				<span class="address">9050 KENAMAR DRIVE<br/>SAN DIEGO, CA 92121</span><!-- address -->
+				<span class="phone">858.271.9400</span><!-- phone -->
+				
+				<a href="" class="get_directions">Get Directions</a>
+				
+			</div><!-- location -->
+			
+			<div class="location">
+				
+				<span class="address">9050 KENAMAR DRIVE<br/>SAN DIEGO, CA 92121</span><!-- address -->
+				<span class="phone">858.271.9400</span><!-- phone -->
+				
+				<a href="" class="get_directions">Get Directions</a>
+				
+			</div><!-- location -->
+			
+		</div><!-- locations -->
+		
+	</div><!-- inner_footer -->
+	
+</footer>
 	
 <?php wp_footer(); ?>
 
--- wp-content/themes/greathouse/page-main.php
+++ wp-content/themes/greathouse/page-main.php
@@ -196,6 +196,49 @@ get_header(); ?>
 	
 </section><!-- new_arrivals_wrapper -->
 
+<section class="custom_design_services">
+	
+	<span class="sub_header">Custom DEsign Services</span><!-- large_header -->
+		
+		<div class="blue_block"></div><!-- blue_block -->
+		
+		<span class="large_header">personalize your space</span><!-- large_header -->
+		
+		<a href="" class="white_button" style="border:1px solid #000;">Free Instore Design Consultation</a><!-- white_button -->
+		
+</section><!-- custom_design_services -->
+
+
+<section class="life_well_lived">
+	
+	<span class="large_header">A Life Well Lived Begins With a GREATHOUSE</span><!-- large_header -->
+	
+	<p>We invite you to visit one of our award winning showrooms (San Diego & Carlsbad) to discover a home of possibilities.</p> 
+
+	<p>Greathouse features southern California's best selection of quality outdoor patio furniture. Whether you live along the coast or inland, we have the furnishings to create comfortable and customizable outdoor spaces.  Featuring brands such as: Mallin, Patio Republic, Lloyd Flanders, Ratana, Telescope, Alumont, Lane Venture and more.</p>
+
+	<p>As you tour the showroom you will discover San Diego's most unique and diverse selection of custom upholstery, sofas and sectionals.  With over one thousand designer fabrics to choose from you are sure to find the look that is uniquely yours.  Highlighting the showroom are name brands such as: Lexington, Stanley, Jonathan Adler, Teodora, Cisco Brothers, Classic Home, Camerich along with many others.</p>
+	
+
+<span class="quote">“let yourself in”<br/>- greathouse</span>
+
+	
+</section><!-- life_well_lived -->
+
+<section class="keyholder">
+	
+	<div class="inner_keyholder">
+	
+		<span class="sub_header">become a member</span><!-- sub_header -->
+		<span class="large_header">Keyholder Club</span><!-- sub_header -->
+		
+		<a href="" class="transparent_button">Sign Up Today</a>
+	
+	
+	</div><!-- inner_keyholder -->
+	
+</section><!-- keyholder -->
+
 
 
 <?php get_footer(); ?>
