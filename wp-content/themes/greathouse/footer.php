6950000a89cdc20a44c0f21b7281c1f434d46c13
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-14 10:29:26 -0700

    push

--- wp-content/themes/greathouse/footer.php
+++ wp-content/themes/greathouse/footer.php
@@ -11,23 +11,69 @@
 			
 			<div class="location">
 				
-				<span class="address">9050 KENAMAR DRIVE<br/>SAN DIEGO, CA 92121</span><!-- address -->
-				<span class="phone">858.271.9400</span><!-- phone -->
+				<a href="https://www.google.com/maps/place/9050+Kenamar+Dr,+San+Diego,+CA+92121/@32.8869989,-117.1580936,17z/data=!3m1!4b1!4m5!3m4!1s0x80dbf8b3f559e50b:0xf8969d11a3f3e4a!8m2!3d32.8869989!4d-117.1559049" class="address" target="_blank">9050 KENAMAR DRIVE<br/>SAN DIEGO, CA 92121</a><!-- address -->
+				<a href="" class="phone">858.271.9400</a><!-- phone -->
 				
-				<a href="" class="get_directions">Get Directions</a>
+				<a href="https://www.google.com/maps/place/9050+Kenamar+Dr,+San+Diego,+CA+92121/@32.8869989,-117.1580936,17z/data=!3m1!4b1!4m5!3m4!1s0x80dbf8b3f559e50b:0xf8969d11a3f3e4a!8m2!3d32.8869989!4d-117.1559049" class="get_directions" target="_blank">Get Directions</a>
 				
 			</div><!-- location -->
 			
 			<div class="location">
 				
-				<span class="address">9050 KENAMAR DRIVE<br/>SAN DIEGO, CA 92121</span><!-- address -->
-				<span class="phone">858.271.9400</span><!-- phone -->
+				<a href="https://www.google.com/maps/place/7720+El+Camino+Real,+Carlsbad,+CA+92009/data=!4m2!3m1!1s0x80dc0b61c94063b3:0xa2e40bb5cdce4feb?sa=X&ved=0ahUKEwjl5oyHqY_PAhUYzWMKHY-UB2wQ8gEIHTAA" class="address" target="_blank">7720 EL CAMINO REAL<br/>CARLSBAD, CA 92009</a><!-- address -->
+				<a href="" class="phone">760.454.0013</a><!-- phone -->
 				
-				<a href="" class="get_directions">Get Directions</a>
+				<a href="https://www.google.com/maps/place/7720+El+Camino+Real,+Carlsbad,+CA+92009/data=!4m2!3m1!1s0x80dc0b61c94063b3:0xa2e40bb5cdce4feb?sa=X&ved=0ahUKEwjl5oyHqY_PAhUYzWMKHY-UB2wQ8gEIHTAA" class="get_directions" target="_blank">Get Directions</a>
 				
 			</div><!-- location -->
 			
 		</div><!-- locations -->
+		
+		<div class="follow_us">
+			
+			<span class="large_header">follow us</span><!-- large_header -->
+			
+			<div class="social_icons">
+				
+				<a href="" target="_blank">
+					<img class="social_icon" src="<?php bloginfo('template_directory');?>/images/social_fb.svg"/>
+				</a>
+				
+				<a href="" target="_blank">
+					<img class="social_icon" src="<?php bloginfo('template_directory');?>/images/social_twitter.svg"/>
+				</a>
+				
+				<a href="" target="_blank">
+					<img class="social_icon" src="<?php bloginfo('template_directory');?>/images/social_pinterest.svg"/>
+				</a>
+				
+				<a href="" target="_blank">
+					<img class="social_icon" src="<?php bloginfo('template_directory');?>/images/social_yelp.svg"/>
+				</a>
+				
+				<a href="" target="_blank">
+					<img class="social_icon" src="<?php bloginfo('template_directory');?>/images/social_instagram.svg"/>
+				</a>
+				
+				<a href="" target="_blank">
+					<img class="social_icon" src="<?php bloginfo('template_directory');?>/images/social_google.svg"/>
+				</a>
+				
+			</div><!-- social_icons -->
+			
+		</div><!-- follow_us -->
+		
+		<div class="footer_menu">
+			
+			<ul>
+				<li><a href="">Shop</a></li>
+				<li><a href="">DESIGN SERVICES</a></li>
+				<li><a href="">contract sales</a></li>
+				<li><a href="">national shipping</a></li>
+				<li><a href="">blog</a></li>
+			</ul>
+			
+		</div><!-- footer_menu -->
 		
 	</div><!-- inner_footer -->
 	
