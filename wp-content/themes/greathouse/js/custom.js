43b6aac059ea16a9e922676f0795c766908bc024
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-20 15:48:13 -0700

    push

--- wp-content/themes/greathouse/js/custom.js
+++ wp-content/themes/greathouse/js/custom.js
@@ -1,4 +1,23 @@
 jQuery(document).ready(function(){
+  
+  
+  
+
+ //jQuery("ul#menu-menu-1 > li > ul.sub-menu > li").addClass("garrett");
+ 
+
+var divs = jQuery("ul#menu-menu-1 > li > ul.sub-menu > li");
+for(var i = 0; i < divs.length; i+=2) {
+  divs.slice(i, i+2).wrapAll("<div class='menu_col'></div>");
+}
+
+
+
+  
+  
+  
+  // New Arrivals
+  
   jQuery('.new_arrival_slideshow').slick({
     prevArrow: ".prev",
     nextArrow: ".next",
@@ -21,5 +40,8 @@ jQuery(document).ready(function(){
     }
   ]
   });
-});
+
+
+
+}); // Document Ready
 		
