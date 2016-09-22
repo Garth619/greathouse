47c3feee73281ee06ab3450404c65454320fd37b
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-22 14:05:06 -0700

    push

---
+++ wp-content/themes/greathouse/page-nosidebar.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: No Sidebar Inner Page
+ 
+ */
+
+get_header(); ?>
+
+
+<div class="banner">
+	
+	<span class="">Designs Services</span>
+	
+	
+</div><!-- banner -->
+
+
+<div class="no_sidebar_wrapper">
+	
+	<div class="no_sidebar_inner">
+	
+		<span class="large_header">More than just a furniture store</span><!-- large_header -->
+		
+		<div class="no_sidebar_content">
+	
+			<div id="content">
+			
+				<p>The moment you enter one of our award winning showrooms you will be treated to a completely different shopping experience. As you stroll through, you will see some of the most trusted designer brands in the industry and uncover San Diego's most unique and diverse selection of custom upholstery, one-of-a kind imports and outdoor patio furniture. You will unearth hidden treasures and accessories to consummate your look and discover a home of possibilities.</p>
+
+				<p>Shop with confidence knowing that our experienced and knowledgeable design staff can assist you in finding the right product and décor to suit your lifestyle. The Greathouse team features the most knowledgeable, en pointe, and friendly design staff in Southern California. With over a century of combined design & home furnishings experience, you are sure to find the professional that is right for you. Let's meet the staff, shall we?</p>
+			
+			</div><!-- content -->
+	
+		</div><!-- no_sidebar_content -->
+	
+	</div><!-- no_sidebar_inner -->
+		
+</div><!-- no_sidebar_wrapper -->
+
+		
+<?php get_footer(); ?>
+
+
+
+
+	<?php // get_template_part( 'loop', 'page' ); ?>
