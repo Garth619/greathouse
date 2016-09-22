6721446025082c7fb17fc328fb039d541f6465da
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-22 16:17:33 -0700

    push

--- wp-content/themes/greathouse/page-nosidebar.php
+++ wp-content/themes/greathouse/page-nosidebar.php
@@ -21,7 +21,7 @@ get_header(); ?>
 
 	<div class="no_sidebar_inner">
 	
-		<span class="large_header">More than just a furniture store</span><!-- large_header -->
+		<span class="large_header_no_sidebar">More than just a furniture store</span><!-- large_header -->
 		
 		<div class="no_sidebar_content">
 	
@@ -30,6 +30,219 @@ get_header(); ?>
 				<p>The moment you enter one of our award winning showrooms you will be treated to a completely different shopping experience. As you stroll through, you will see some of the most trusted designer brands in the industry and uncover San Diego's most unique and diverse selection of custom upholstery, one-of-a kind imports and outdoor patio furniture. You will unearth hidden treasures and accessories to consummate your look and discover a home of possibilities.</p>
 
 				<p>Shop with confidence knowing that our experienced and knowledgeable design staff can assist you in finding the right product and décor to suit your lifestyle. The Greathouse team features the most knowledgeable, en pointe, and friendly design staff in Southern California. With over a century of combined design & home furnishings experience, you are sure to find the professional that is right for you. Let's meet the staff, shall we?</p>
+				
+				
+				<section class="team_wrapper">
+					
+					
+					
+					
+					
+					<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+			
+			
+			<div class="single_team_square">
+		
+						<a href="">
+		
+						<div class="team_overlay">
+			
+							<div class="team_inner_wrapper">
+				
+								<span class="large_header">Sandi Lahage</span><!-- large_header -->
+								<span class="sub_header">designer</span><!-- sub_header -->
+				
+							</div><!-- product_inner_wrapper -->
+			
+						</div><!-- product_overlay -->
+		
+						<div class="team_background" style="background: rgba(0, 0, 0, 0) url(<?php bloginfo('template_directory');?>/images/designer.jpg) no-repeat scroll left top / cover ;"></div><!-- product_background -->
+		
+					</a>
+	
+			</div><!-- single_product_square -->
+					
+					
+					
+				
+				
+				
+				
+				</section><!-- team_wrapper -->
+				
 			
 			</div><!-- content -->
 	
